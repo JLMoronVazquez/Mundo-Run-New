0c5ec968d2c95b2ddd09d2334c501024de9b4377
juan <juampats12345@gmail.com>
2024-07-03 18:37:06 +0200

    character added to player v3

--- Assets/Scripts/RotateWorld.cs
+++ Assets/Scripts/RotateWorld.cs
@@ -7,6 +7,7 @@ public class RotateWorld : MonoBehaviour
     public float normalSpeed, rollSpeed, tripSpeed, carSpeed, boatSpeed, stopSpeed;
     public Transform player;
     public LayerMask layerObstacles;
+    public LayerMask layerBrake;
 
     [HideInInspector] public float speedRot;
     private Rigidbody rb;
@@ -22,6 +23,11 @@ public class RotateWorld : MonoBehaviour
     void FixedUpdate()
     {
         //transform.Rotate(new Vector3(-speedRot, 0f, 0f) * Time.deltaTime);
+        speedRot = normalSpeed;
+        if(dirtUnderPlayer())
+        {
+            speedRot = tripSpeed;
+        }
 
         Quaternion deltaRotation = Quaternion.Euler(new Vector3(-speedRot, 0f, 0f) * Time.deltaTime);
         if (!ObstacleAheadPlayer())
@@ -36,9 +42,22 @@ public class RotateWorld : MonoBehaviour
     private bool ObstacleAheadPlayer()
     {
         RaycastHit hit;
-        Vector3 posOrogin = player.transform.position + Vector3.up*0.25f;
-        Debug.DrawRay(posOrogin, player.transform.TransformDirection(Vector3.forward), Color.red);
-        if (Physics.Raycast(posOrogin, player.transform.TransformDirection(Vector3.forward), out hit, 0.4f, layerObstacles))
+        Vector3 posOrogin = transform.position + Vector3.up*0.25f;
+        Debug.DrawRay(posOrogin, transform.TransformDirection(Vector3.forward), Color.red);
+        if (Physics.Raycast(posOrogin, transform.TransformDirection(Vector3.forward), out hit, 0.4f, layerObstacles))
+        {
+            //print(hit.collider.gameObject.name);
+            return true;
+        }
+        return false;
+    }
+
+    private bool dirtUnderPlayer()
+    {
+        RaycastHit hit;
+        Vector3 posOrogin = transform.position + Vector3.up*0.25f;
+        Debug.DrawRay(posOrogin, transform.TransformDirection(Vector3.down), Color.blue);
+        if (Physics.Raycast(posOrogin, transform.TransformDirection(Vector3.down), out hit, 0.4f, layerBrake))
         {
             //print(hit.collider.gameObject.name);
             return true;
