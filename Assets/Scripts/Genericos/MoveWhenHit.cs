154ed1fafe0ca0c3c22660c4d99a4e4fbb628e15
juan <juampats12345@gmail.com>
2024-08-14 20:16:18 +0200

    script para mover cosas al ser golpeadas por el paquete

---
+++ Assets/Scripts/Genericos/MoveWhenHit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MoveWhenHit : MonoBehaviour
+{
+    private bool hasMoved;
+    public float timeToMove;
+    public Transform targetPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasMoved = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if( !hasMoved && collision.gameObject.CompareTag("Paquete"))
+        {
+            hasMoved = true;
+            transform.DOMove( targetPos.position, timeToMove );
+        }
+    }
+}
