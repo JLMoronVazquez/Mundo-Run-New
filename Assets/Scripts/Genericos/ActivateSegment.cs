1d036790c1e76fdc7486bd7b40f2cea4c81100e9
juan <juampats12345@gmail.com>
2024-09-17 18:25:32 +0200

    fix script

--- Assets/Scripts/Genericos/ActivateSegment.cs
+++ Assets/Scripts/Genericos/ActivateSegment.cs
@@ -6,7 +6,7 @@ public class ActivateSegment : MonoBehaviour
 {
     public GameObject segment;
 
-    public void OnTriggerEnter(Collider2D col)
+    public void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
