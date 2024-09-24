4625c1efb95ae4ce21f4399aba0f618ac8e80b77
juan <juampats12345@gmail.com>
2024-09-24 18:41:07 +0200

    mano ya no da en la hebilla

--- Assets/Scripts/UI/LeftArm.cs
+++ Assets/Scripts/UI/LeftArm.cs
@@ -7,40 +7,62 @@ public class LeftArm : Arm
 {
     public float responsivenes;
     public Vector4 limits;
+    public float clickDuration;
+    public float clickDeep;
+
+    private Vector3 positionPreClick;
 
     public void Update()
     {
         if (isActivated)
         {
-            //Input.GetAxis("Mouse X")
-            Vector3 mouseMov = transform.localPosition;
-            mouseMov.x += Input.GetAxis("Mouse X");
-            mouseMov.y += Input.GetAxis("Mouse Y");
-            CheckLimitis( ref mouseMov );
-            transform.DOLocalMove(mouseMov, responsivenes).SetUpdate(true);
+            HoverHand();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Click();
+            }
         }
     }
 
 
-    public void CheckLimitis( ref Vector3 mouseMov )
+    public void Click()
+    {
+        positionPreClick = transform.localPosition;
+        Vector3 posClick = transform.localPosition;
+        posClick.z += clickDeep;
+        transform.DOLocalMove(posClick, clickDuration).SetUpdate(true);
+    }
+
+
+    public void HoverHand()
+    {
+        Vector3 mouseMov = transform.localPosition;
+        mouseMov.x += Input.GetAxis("Mouse X");
+        mouseMov.y += Input.GetAxis("Mouse Y");
+        CheckLimitis(ref mouseMov);
+        transform.DOLocalMove(mouseMov, responsivenes).SetUpdate(true);
+    }
+
+
+    public void CheckLimitis(ref Vector3 mouseMov)
     {
         //print("posicion: " + mouseMov.x);
-        if( mouseMov.x > limits.x )
+        if (mouseMov.x > limits.x)
         {
             mouseMov.x = limits.x;
         }
 
-        if( mouseMov.x < limits.y )
+        if (mouseMov.x < limits.y)
         {
             mouseMov.x = limits.y;
         }
 
-        if( mouseMov.y > limits.z )
+        if (mouseMov.y > limits.z)
         {
             mouseMov.y = limits.z;
         }
 
-        if( mouseMov.y < limits.w )
+        if (mouseMov.y < limits.w)
         {
             mouseMov.y = limits.w;
         }
