5043ea4d9567392dc634f4db8af21d5f023a0590
juan <juampats12345@gmail.com>
2024-10-02 18:41:52 +0200

    otro fix movWhenHit

--- Assets/Scripts/Genericos/MoveWhenHit.cs
+++ Assets/Scripts/Genericos/MoveWhenHit.cs
@@ -15,7 +15,7 @@ public class MoveWhenHit : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         originalPos = transform.localPosition;
         originalRotation = transform.localEulerAngles;
